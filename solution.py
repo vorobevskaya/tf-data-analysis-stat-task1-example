164774fab36edffe156c49bc94c87ca412acde8c
vorobevskaya <113284505+vorobevskaya@users.noreply.github.com>
2023-04-04 00:14:28 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,8 +5,7 @@ import numpy as np
 chat_id = 689606612 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    coef = x/63
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return coef.mean() # Ваш ответ
+    return 63*np.mean(x) # Ваш ответ
