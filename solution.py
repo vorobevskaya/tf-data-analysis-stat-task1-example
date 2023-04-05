a91c664f7c4f2235a5a7b588f4e6fbf59ea7edb9
vorobevskaya <113284505+vorobevskaya@users.noreply.github.com>
2023-04-05 17:39:07 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,4 +8,10 @@ def solution(x: np.array) -> float:
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return 63*np.mean(x) # Ваш ответ
+    n = len(x)
+    x_mean = x.mean()
+    summ = 0
+    for i in range(n):
+      summ += (x[i]-x_mean)**2
+    result = np.sqrt(2*(n-1)*summ)
+    return result # Ваш ответ
